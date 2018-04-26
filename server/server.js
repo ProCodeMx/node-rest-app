765897485695912cef9a338b45a7f8ccc8cab824
ProCodeMx <victormx93x@gmail.com>
2018-04-26 17:25:57 -0600

    Initial Project

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+//const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
+require('./config/config');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/users', (req, res) => {
+    res.send("hello world")
+});
+
+app.post('/users', (req, res) => {
+    let body = req.body;
+
+    res.json({ body })
+});
+
+app.put('/users/:id', (req, res) => {
+    res.json("put users")
+});
+
+app.delete('/users', (req, res) => {
+    res.json("delete users")
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`App Listening on port ${process.env.PORT}`);
+})
